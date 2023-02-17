0906d2bac5f505e79971b9ab860e6e55cc9d58df
Ivan <clicker.heroes.acg@gmail.com>
2023-02-17 22:21:00 +0300

    Initial commit

---
+++ src/main/java/com/example/mucontrol/MuControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.mucontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mucontrol/MuControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mucontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
